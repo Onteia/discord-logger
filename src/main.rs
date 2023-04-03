4dff6bbea461db356b38922313a1ce80fb81e669
Onteia <yeyroy@gmail.com>
2023-04-03 04:05:37 -0700

    log with message embed

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,31 @@
 use std::fs;
 use serenity::async_trait;
 use serenity::prelude::*;
+use serenity::Client;
+use serenity::model::prelude::Ready;
 use serenity::model::channel::Message;
+use serenity::model::gateway::Activity;
 
+
+const DISCORD_AUTH_PATH: &'static str = "discord.auth";
 
 struct Handler;
+
+struct Server {
+    guild_id: String,
+    channel_id: String
+}
 
 #[async_trait]
 impl EventHandler for Handler {
 
+    //when MessageLogger starts
+    async fn ready(&self, ctx: Context, _data_about_bot: Ready) {
+        let activity = Activity::playing("!setuplogging");
+        ctx.set_activity(activity);
+    }
+
+    // when a user sends a message
     async fn message(&self, ctx: Context, msg: Message) {
         
         //ignore messages from MessageLogger
@@ -17,10 +34,41 @@ impl EventHandler for Handler {
             return;
         }
 
+        if msg.content.starts_with("!setuplogging") {
+            
+            //save channel id and associate with guild id
+            println!("guildID: {} \nchannelID: {}", msg.guild_id.unwrap(), msg.channel_id);
+
+            
+
+            return;
+
+        }
+
+
+
+
+        //ignore messages if logging not set up
+
+        //read log file associated with channel id
+        
+        //channel to send the log message to
+        //let log_channel: ChannelId;
+
+        let author = &msg.author.name;
+        let link = &msg.link();
+
+
         msg.channel_id.send_message(
             &ctx, 
             |reply| {
-                reply.content(msg.content)
+                reply.add_embed(|e| {
+                    e.title(author);
+                    e.url(link);
+                    e.description("#channel");
+                    e.field("posted:", msg.content, false);
+                    e.timestamp(msg.timestamp)
+                })
             })
             .await
             .unwrap();
@@ -29,10 +77,20 @@ impl EventHandler for Handler {
 
 }
 
-const DISCORD_AUTH_PATH: &'static str = "discord.auth";
+
 
 #[tokio::main]
 async fn main() {   
+    
+    setup_bot().await;
+
+    //read json file
+
+    //get info of discord server and log channel pairs
+
+}
+
+async fn setup_bot() {
     //get the token from file
     let token = fs::read_to_string(DISCORD_AUTH_PATH).expect("could not read discord token file!");
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
@@ -41,10 +99,9 @@ async fn main() {
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .await
-        .expect("error creating client!");
+        .expect("error creating client!"); 
 
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
-
 }
