bfd8c8446f1e33e4b7032cad2caaccd42f2e7dd0
Onteia <yeyroy@gmail.com>
2023-04-16 21:36:41 -0700

    msg author icon in log, change bot check to tag instead of name

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,42 @@ use std::collections::HashMap;
 use serenity::async_trait;
 use serenity::model::Timestamp;
 use serenity::model::prelude::MessageUpdateEvent;
+use serenity::model::application::command::Command;
+use serenity::model::application::interaction::Interaction;
+use serenity::model::application::interaction::InteractionType;
 use serenity::prelude::*;
 use serenity::Client;
+use serenity::model::permissions::Permissions;
 use serenity::model::prelude::Ready;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Activity;
 use serenity::utils::Color;
 use serde::{Serialize, Deserialize};
+use serde_json::Error;
 
 
 const DISCORD_AUTH_PATH: &'static str = "discord.auth";
 const JSON_PATH: &'static str = "./servers.json";
-const LOGGER_NAME: &'static str = "MessageLogger";
+const LOGGER_TAG: &'static str = "MessageLogger#0584";
 
 /*TODO: 
+    
+    ~change bot check to use tag
+    ~include pfp in log
+
     properly log embeds
-    log message editing
     possibly proper gif rendering
         because the gifs are cached and they could get removed from the cdn
+    proper attachment logging
+
+    add a command to remove logging
+    delete log of server when bot is kicked
+
+    show the user's pfp in the embed
+
+    color hash with full discord tag
+
+
 */
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -41,77 +59,85 @@ impl EventHandler for Handler {
 
     //when MessageLogger starts
     async fn ready(&self, ctx: Context, _data_about_bot: Ready) {
-        let activity = Activity::playing("!setuplogging");
+        let activity = Activity::playing("/setuplogging");
         ctx.set_activity(activity).await;
+    
+        let _setuplogging = Command::create_global_application_command(&ctx, |command| {
+            command.name("setuplogging");
+            command.description("setup logging for this channel");
+            command.default_member_permissions(Permissions::MANAGE_GUILD)
+        }).await;
+    
     }
     
+    //handle interactions
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+
+        //handle slash commands
+        if interaction.kind() == InteractionType::ApplicationCommand {
+            let slash_command = interaction.application_command()
+                .expect("interaction_create(): unable to convert the interaction 
+                to an application command!");
+            
+            if slash_command.data.name == "setuplogging" {
+                //get current channel id
+                let c_id = *slash_command.channel_id.as_u64();
+                let g_id_str = slash_command.guild_id
+                    .expect("interaction_create(): unable to get the guild_id!")
+                    .to_string();
+
+                //update the save_map with the new server,channel pair
+                let mut save_map = read_json();
+                save_map.insert(g_id_str, c_id);
+                
+                write_json(&save_map)
+                    .expect("interaction_create(): unable to write to the json file!");
+
+                slash_command
+                    .create_interaction_response(
+                        &ctx, 
+                        |reply| {
+                            reply.interaction_response_data(|message| {
+                                message.content("logging has been successfully set up for this channel!")
+                    })
+                })
+                .await
+                .unwrap();
+
+                return;
+            }
+        }
+
+    }
+
     //when a user sends a message
     async fn message(&self, ctx: Context, msg: Message) {
+        let author = msg.author.clone();
 
         //ignore messages from MessageLogger
-        if msg.author.bot && msg.author.name == LOGGER_NAME {
+        if author.bot && author.tag() == LOGGER_TAG {
             return;
         }
 
         let g_id = msg.guild_id
-            .expect("message(): unable to get the guild_id!")
-            .to_string();
+            .expect("message(): unable to get the guild_id!");
+        let g_id_str = g_id.to_string();
 
-        //read from json file
-        let contents = fs::read_to_string(JSON_PATH)
-            .expect("message(): unable to read the json file!");
+        let save_map = read_json();
         
-        let mut save_map: HashMap<String, u64>;
-        //if the json file is empty, initialize the hash map
-        //deserialize existing json file otherwise
-        if contents == "" {
-            save_map = HashMap::new();
-        } else {
-            save_map = serde_json::from_str::<SaveMap>(&contents)
-                .expect("message(): unable to convert the json file to a SaveMap!").map;
-        }
-
-        if msg.content.starts_with("!setuplogging") {
-            //get current channel id
-            let c_id = *msg.channel_id.as_u64();
-            
-            //update the save_map with the new server,channel pair
-            save_map.insert(g_id, c_id);
-            
-            //serialize and write to the json file
-            let serialized = serde_json::to_string(&save_map)
-                .expect("!setuplogging: unable to serialize the save_map!");
-            fs::write(JSON_PATH, &serialized)
-                .expect("!setuplogging: unable to write the serializable object to the json file!");
-
-            //send confirmation message
-            msg.channel_id
-                .send_message(
-                    &ctx, 
-                    |reply| {
-                        reply.content("logging has been successfully set up for this channel!")
-                    })
-                .await
-                .unwrap();
-
-            return;
-
-        }
-
         //ignore messages if logging not set up
-        if !save_map.contains_key(&g_id) {
+        if !save_map.contains_key(&g_id_str) {
             return;
         }
 
         //get the channel id associated with the guild id
-        let c_id = *save_map.get(&g_id)
+        let c_id = *save_map.get(&g_id_str)
             .expect("message(): unable to get the channel id from the hash map!");
         //turn the c_id into a guild channel
         let log_channel = ctx.http.get_channel(c_id).await
             .expect("message(): unable to get the channel!")
             .guild().expect("message(): unable to get the guild channel!");
         
-        let author = msg.author.name.clone();
         let link = msg.link();
         
         //get the channel name to format it as: `#channel_name` in the embed
@@ -120,18 +146,21 @@ impl EventHandler for Handler {
             .guild().expect("message(): unable to get the guild from the channel!");
         let channel_name = "#".to_owned() + guild_channel.name();
         let time = msg.timestamp;
-        let display_color = color_hash(&channel_name, &author, time);
+        let display_color = color_hash(&channel_name, &author.tag(), time);
+        let author_icon_url = author.avatar_url()
+            .expect("message(): unable to obtain author's profile url!");
 
         log_channel.send_message(
             &ctx, 
             |reply| {
                 reply.add_embed(|e| {
-                    e.title(author);
+                    e.title(author.tag());
                     e.url(link);
                     e.description(channel_name);
                     e.field("posted:", msg.content, false);
                     e.timestamp(time);
-                    e.color(Color::new(display_color))
+                    e.color(Color::new(display_color));
+                    e.thumbnail(author_icon_url)
                 })
             })
         .await
@@ -145,28 +174,16 @@ impl EventHandler for Handler {
             .expect("message_update(): unable to get author!");
 
         //ignore messages from MessageLogger
-        if author.bot && author.name == LOGGER_NAME {
+        if author.bot && author.tag() == LOGGER_TAG {
             return;
         }
 
         let g_id = updated.guild_id
             .expect("message_update(): unable to get the guild_id!")
             .to_string();
-
-        //read from json file
-        let contents = fs::read_to_string(JSON_PATH)
-            .expect("message_update(): unable to read the json file!");
         
-        let save_map: HashMap<String, u64>;
-        //if the json file is empty, initialize the hash map
-        //deserialize existing json file otherwise
-        if contents == "" {
-            save_map = HashMap::new();
-        } else {
-            save_map = serde_json::from_str::<SaveMap>(&contents)
-                .expect("message_update(): unable to convert the json file to a SaveMap!").map;
-        }
-
+        let save_map = read_json();
+        
         //ignore messages if logging not set up
         if !save_map.contains_key(&g_id) {
             return;
@@ -180,7 +197,6 @@ impl EventHandler for Handler {
             .expect("message_update(): unable to get the channel!")
             .guild().expect("message_update(): unable to get the guild channel!");
         
-        let author = author.name;
         let link = "https://discord.com/channels/".to_owned() 
             + &g_id + "/" + c_id.to_string().as_str() 
             + "/" + updated.id.to_string().as_str();
@@ -194,19 +210,25 @@ impl EventHandler for Handler {
         let channel_name = "#".to_owned() + guild_channel.name();
         let updated_text = updated.content
             .expect("message_update(): unable to get the updated message!");
-        let time = updated.timestamp.unwrap();
-        let display_color = color_hash(&channel_name, &author, time);
+        let time = updated.timestamp
+            .expect("message_update(): unable to get timestamp of original message!");
+        let display_color = color_hash(&channel_name, &author.tag(), time);
+        let edited_time = updated.edited_timestamp
+            .expect("message_update(): unable to get timestamp of edited message!");
+        let author_icon_link = author.avatar_url()
+            .expect("message_update(): unable to obtain author's profile url!");
 
         log_channel.send_message(
             &ctx, 
             |reply| {
                 reply.add_embed(|e| {
-                    e.title(author);
+                    e.title(author.tag());
                     e.url(link);
                     e.description(channel_name);
                     e.field("edited:", updated_text, false);
-                    e.timestamp(updated.edited_timestamp.unwrap());
-                    e.color(Color::new(display_color))
+                    e.timestamp(edited_time);
+                    e.color(Color::new(display_color));
+                    e.thumbnail(author_icon_link)
                 })
             })
         .await
@@ -214,6 +236,7 @@ impl EventHandler for Handler {
 
         
     }
+
 
 }
 
@@ -236,7 +259,7 @@ async fn main() {
 async fn setup_bot() {
     //get the token from file
     let token = fs::read_to_string(DISCORD_AUTH_PATH).expect("could not read discord token file!");
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MEMBERS;
 
     //build the client
     let mut client = Client::builder(token, intents)
@@ -249,14 +272,39 @@ async fn setup_bot() {
     }
 }
 
+fn read_json() -> HashMap<String, u64> {
+    //read from json file
+    let contents = fs::read_to_string(JSON_PATH)
+        .expect("read_json(): unable to read the json file!");
+
+    let map: Result<SaveMap, Error>;
+    //if the json file is empty, initialize the hash map
+    //deserialize existing json file otherwise
+    if contents == "" {
+        let save_map = SaveMap{ map: HashMap::new() };
+        map = Ok(save_map);
+    } else {
+        map = serde_json::from_str::<SaveMap>(&contents);
+    }
+    map.expect("read_json(): unable to convert the json file to a SaveMap!").map
+}
+
+fn write_json(save_map: &HashMap<String, u64>) -> Result<(), std::io::Error> {
+    //serialize and write to the json file
+    let serialized = serde_json::to_string(&save_map)
+        .expect("write_json(): unable to serialize the save_map!");
+    fs::write(JSON_PATH, &serialized)?;
+    Ok(())
+}
+
 //turn (#channel, @user, timestamp) into a color
-fn color_hash(channel_name: &String, username: &String, time: Timestamp) -> u32 {
-    
+fn color_hash(channel_name: &String, user: &String, time: Timestamp) -> u32 {
+   
     const TIMESTAMP_WEIGHT: u32 = 100;
 
     let mut hasher = DefaultHasher::new();
     channel_name.hash(&mut hasher);
-    username.hash(&mut hasher);
+    user.hash(&mut hasher);
 
     //get the number of seconds since the beginning of the day
     const SECONDS_IN_DAY: i64 = 86400;
@@ -268,6 +316,7 @@ fn color_hash(channel_name: &String, username: &String, time: Timestamp) -> u32 
 
     //hash so the color value increases linearly with the timestamp
     let hashed_val = (hasher.finish() as u32) + (TIMESTAMP_WEIGHT * secs_of_day);
+    
     //get it as a u24 (get rid of first 8 bits)
     let result = hashed_val & 0x00FFFFFF;
     result
